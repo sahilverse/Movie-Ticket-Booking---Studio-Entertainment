5d6dff96f2131885f12e7773b5c348585a3c4cc5
Sahil Dahal <denenme49@gmail.com>
2024-09-21 23:02:38 +0545

    minor changes - css and ui -

--- src/components/ComingSoon/ComingSoon.tsx
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -2,42 +2,36 @@ import React from 'react';
 import styles from './ComingSoon.module.css'
 
 import Image from "next/image";
+import { MovieCardType } from "@/types/types";
+
+import { useRouter } from 'next/navigation';
 
 import rrrImage from "@/assets/MovieCard/rrr.jpg";
 import jokerImage from "@/assets/MovieCard/joker.jpg";
 import kalkiImage from "@/assets/MovieCard/kalki.jpg";
 
 const ComingSoon = () => {
+
+    const router = useRouter();
+
+    const [movies, setMovies] = React.useState<MovieCardType[]>([
+        { _id: 1, name: "RRR", imageUrl: rrrImage, playTime: "2h 30m", releaseDate: "20 Sept 2024" },
+        { _id: 2, name: "Joker", imageUrl: jokerImage, playTime: "2h 30m", releaseDate: "22 Sept 2024" },
+        { _id: 3, name: "Kalki", imageUrl: kalkiImage, playTime: "2h 30m", releaseDate: "30 Sept 2024" },
+    ]);
     return (
         <div className={styles.container}>
             <p>Coming Soon</p>
 
             <div className={styles.card_container}>
-                <div className={styles.card}>
-                    <Image src={rrrImage} alt="RRR" loading='lazy' className={styles.image} />
-                    <p className={styles.release_date}>13 Oct 2021</p>
+                {movies.map((movie) => (
+                    <div key={movie._id} className={styles.card} onClick={() => router.push(`/coming-soon/${movie._id}`)}>
+                        <Image src={movie.imageUrl} alt={movie.name} loading='lazy' className={styles.image} />
+                        <p className={styles.release_date}>{movie.releaseDate}</p>
 
-                </div>
-                <div className={styles.card}>
-                    <Image src={jokerImage} alt="RRR" loading='lazy' className={styles.image} />
-                    <p className={styles.release_date}>13 Oct 2021</p>
+                    </div>
+                ))}
 
-                </div>
-                <div className={styles.card}>
-                    <Image src={kalkiImage} alt="RRR" loading='lazy' className={styles.image} />
-                    <p className={styles.release_date}>13 Oct 2021</p>
-
-                </div>
-                <div className={styles.card}>
-                    <Image src={rrrImage} alt="RRR" loading='lazy' className={styles.image} />
-                    <p className={styles.release_date}>13 Oct 2021</p>
-
-                </div>
-                <div className={styles.card}>
-                    <Image src={jokerImage} alt="RRR" loading='lazy' className={styles.image} />
-                    <p className={styles.release_date}>13 Oct 2021</p>
-
-                </div>
 
 
 
