c5c3f2d0b2a4c0f60538ed13b7c0809bea6b31bc
Sahil Dahal <denenme49@gmail.com>
2024-10-17 10:32:53 +0545

    Add MovieDetails component

---
+++ src/app/movie-details/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import rrrImage from "@/assets/MovieCard/rrr.jpg";
+import Image from "next/image";
+
+const MovieDetails = () => {
+    return (
+        <section className="main_container mt-24">
+            page starts here
+
+        </section >
+    );
+};
+
+export default MovieDetails;
