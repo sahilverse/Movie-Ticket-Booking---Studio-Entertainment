449df8bd670c4164f2964ba5c10a255ea92f5afc
Sahil Dahal <denenme49@gmail.com>
2024-11-04 11:15:09 +0545

    Refactor Security component: remove unused useState import to streamline code

--- src/components/client/profile/Security.tsx
+++ src/components/client/profile/Security.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useTransition } from 'react'
+import React, { useTransition } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
