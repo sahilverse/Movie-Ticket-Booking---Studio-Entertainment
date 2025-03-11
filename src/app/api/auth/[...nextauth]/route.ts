8da92d5315ef8942b4e40e662a3379f324554464
Sahil Dahal <denenme49@gmail.com>
2025-03-11 16:09:28 +0545

    fix: update runtime configuration export in auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,4 @@
 import { handlers } from "@/app/api/auth/[...nextauth]/auth"
 export const { GET, POST } = handlers
 
-export const config = {
-    runtime: "nodejs",
-};
+export const runtime = "nodejs";
